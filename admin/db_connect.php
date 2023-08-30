d23de6ac04e6246327ca6548f51a6473992b8488
SesethuHotele <141625946+SesethuHotele@users.noreply.github.com>
2023-08-30 11:23:59 +0200

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('devops-dynamo.mysql.database.azure.com','DevOpsDynamo','Mab101@2023','fos_db')or die("Could not connect to mysql".mysqli_error($con));
